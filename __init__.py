7cbaef59c09555add896883e117d1f320ff98a4e
dinatraykova <dina.traykova.13@ucl.ac.uk>
2020-11-25 22:24:52 +0000

    change pdf to prob

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from scipy.stats import multivariate_normal
 
 def get_imodel(model):
     d = {'shift_L0': 0,
-        'shift_Lvar': 1,
+        'shift_Lvar': 1}
     return d[model]
 
 class shift_priors(Likelihood):
@@ -22,21 +22,29 @@ class shift_priors(Likelihood):
            cov = np.array(self.covL)
         else:
            print('Shift model specified does not exist; Choose shift_L0 for Lambda=0 and shift_Lvar for Lambda!=0 model')
-        return multivariate_normal.logpdf(x,means,cov)
+        print(-1./2.*(x-mu).T.dot(invS).dot(x-mu))
+        print(np.log(multivariate_normal.pdf(x,means,cov)/multivariate_normal.pdf(means,means,cov)))
+        #return np.log(multivariate_normal.pdf(x,means,cov)/multivariate_normal.pdf(means,means,cov))
+        return -1./2.*(x-mu).T.dot(invS).dot(x-mu)
 
     def loglkl(self, cosmo, data):
         # self.model must be in param file
 
-        aB0 = cosmo.pars['parameters_smg__1']
-        m   = cosmo.pars['parameters_smg__2']
-        w0  = cosmo.pars['expansion_smg__2']
-        wa  = cosmo.pars['expansion_smg__3']
+        aB0 = data.cosmo_arguments['parameters_smg__1']
+        m   = data.cosmo_arguments['parameters_smg__2']
+        w0  = data.cosmo_arguments['expansion_smg__2']
+        wa  = data.cosmo_arguments['expansion_smg__3']
+
+        #print("a", (aB0,m,w0,wa))
 
         X1 = aB0
         X2 = m*aB0**(1./6.)
         X3 = w0*m**(1./4.)
         X4 = wa*(m**2.)
-         
+        #print("x", (X1,X2,X3,X4))
+        
         X = np.array((X1, X2, X3, X4))
+        imodel = get_imodel(self.model)
+        #print(self.multvar_normal_logpdf(X, imodel))
 
-        return multvar_normal_logpdf(X, imodel)
+        return self.multvar_normal_logpdf(X, imodel)
